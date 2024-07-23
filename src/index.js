1885b05308d5a693dc5f3c1dc3320128fdacc734
Yan Garcia <yangarciavaz@gmail.com>
2024-07-23 09:20:15 -0300

    feat: endpoint users created + 404 exception created

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 const http = require('http');
 
+const users = require('./mocks/users')
+
 const server = http.createServer((request, response) => {
   console.log(`Request method: ${request.method} | Endpoint: ${request.url}`);
 
-  if (request.url === '/users' && request.method === 'GET') {
+  if (request.url === '/' && request.method === 'GET') {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Rota / funcionando`);
+  } else if (request.url === '/users' && request.method === 'GET') {
     response.writeHead(200, { 'Content-Type': 'application/json' });
-    response.end('');
+    response.end(JSON.stringify(users));
+  } else {
+    response.writeHead(404, { 'Content-Type': 'text/html' });
+    response.end(`Cannot ${request.method} ${request.url}`);
   }
 
-  // response.writeHead(200, { 'Content-Type': 'text/html' });
-  // response.end('<h1> Hello </h1>');
 });
 
-server.listen(3000, () => console.log('-- Server started at http://localhost:3000'));
+server.listen(3000, () => console.log('Server started at http://localhost:3000'));
